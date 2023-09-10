f6866ae6f8bd69eede7792ecddfa9c114370b01b
Akashmondal55 <100996344+Akashmondal55@users.noreply.github.com>
2023-09-10 14:41:08 +0530

    Add files via upload

---
+++ BLOG/script1.js
@@ -0,0 +1,7 @@
+const typed =new Typed('.multiple-text',{
+    strings:['FREEDOM FIGHTER', 'POET', 'PHILOSHOPHER', 'PAINTER', 'SONG WRITER'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true
+  });
